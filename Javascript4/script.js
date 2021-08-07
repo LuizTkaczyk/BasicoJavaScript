3ce3ffdda7ca50d4c05d2fc0e3570830750a75f2
LuizTkaczyk <luiz8907@hotmail.com>
2021-08-07 16:30:11 -0300

    Basico JS

--- Javascript4/script.js
+++ Javascript4/script.js
@@ -7,17 +7,18 @@ document.getElementsByClassName('titulo2')[1].innerHTML = "Alterado via classe n
 document.querySelector('#titulo3').innerHTML = "Alterado via querySelector com id"
 document.querySelector('.titulo3').innerHTML = "Alterado via querySelector com classe"
 document.querySelectorAll('.titulo3').innerHTML = "Alterado todos via querySelector com classe"
+document.getElementsByTagName('input')[0].value = 'Preenchido!'
 
 
 
-
+//*************************************************************************************** */
 
 //Number
 var a = 20 // tem acesso em qualquer area do codigo
 let b = 20 // disponível apenas no escopo em que está declarada (se declarada em escopo global ela podera ser usada normalmente)
 const c = 20 //Não muda
 //String
-var d = "20" 
+var d = "20"
 /*
 let exemplo = 'luiz'
 
@@ -26,3 +27,43 @@ if( algo == 3){
     console.log(exemplo)
 }
 */
+
+//************************************************************************************* */
+
+//manipulando classes de um elemento
+function vermelho() {
+    document.querySelector('#exemplo').classList.remove('verde')//removendo classes 
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.add('vermelho') //adiciona a classe vermelho na classe com a id=exemplo
+    document.querySelector('#exemplo').innerHTML = 'Mudou para vermelho'
+
+}
+
+function verde() {
+    document.querySelector('#exemplo').classList.remove('vermelho')//removendo classes 
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.add('verde')//adiciona a classe verde na classe com a id=exemplo
+    document.querySelector('#exemplo').innerHTML = 'Mudou para verde'
+}
+
+function azul() {
+    document.querySelector('#exemplo').classList.remove('verde')//removendo classes 
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.add('azul')//adiciona a classe azul na classe com a id=exemplo
+    document.querySelector('#exemplo').innerHTML = 'Mudou para azul'
+}
+
+//********************************************************************************* */
+
+function trocar() {
+
+    //contains verifica se existe a tag no elemento, neste exemplo 'verde'
+    if (document.querySelector('#btn1').classList.contains('verde')) {
+        document.querySelector('#btn1').classList.remove('verde')
+        document.querySelector('#btn1').classList.add('vermelho')
+    } else {
+        document.querySelector('#btn1').classList.remove('vermelho')
+        document.querySelector('#btn1').classList.add('verde')
+    }
+
+}
