522eb8e31dbe602bb5523e0d2d448c062ad655be
LuizTkaczyk <luiz8907@hotmail.com>
2021-08-17 18:47:20 -0300

    Site de pizzas

---
+++ Javascript22/script.js
@@ -0,0 +1,68 @@
+let modalQt = 1
+
+//variaveis para uso do query selector ( não necessitando digitar a linha inteira sempre que for usar)
+const c = (el) => document.querySelector(el)
+const cs = (el) => document.querySelectorAll(el)
+
+//Listagem das pizzas
+pizzaJson.map((item, index) => {
+    let pizzaItem = c('.models .pizza-item').cloneNode(true)
+
+
+    pizzaItem.setAttribute('data-key', index)
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault()
+
+        //adicionar as informações no modal
+        let key = e.target.closest('.pizza-item').getAttribute('data-key')
+        modalQt = 1
+
+        c('.pizzaBig img').src = pizzaJson[key].img
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+
+        //removendo o selected do item
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+
+        //percorrendo os tamanhos da pizza
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            //tornando o tamanho grande selecionado como padrão
+            if (sizeIndex == 2) {
+                size.classList.add('selected')
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+        })
+        //padrão de 1 pizza adicionada no carrinho
+        c('.pizzaInfo--qt').innerHTML = modalQt
+
+
+        //abrindo o modal com uma pequena transição
+
+        c('.pizzaWindowArea').style.opacity = 0
+        c('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(() => {
+            c('.pizzaWindowArea').style.opacity = 1
+        }, 200)
+
+    })
+
+    //preencher as informações em pizza item
+    c('.pizza-area').append(pizzaItem)
+})
+
+//Eventos do modal
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal) //atenção!! ao chamar a função não usar os parenteses
+})
