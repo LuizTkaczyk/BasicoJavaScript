93ec6d567b23e1573cc65c4f19f999fdff190a41
LuizTkaczyk <luiz8907@hotmail.com>
2021-08-16 21:20:29 -0300

    Async e await

---
+++ Javascript19/script.js
@@ -0,0 +1,19 @@
+//Async e Await
+
+async function loadPost() {
+    //await: aguarda o resultado e depois continua
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let json = await req.json()
+
+    montarBlog(json)
+}
+
+function montarBlog(lista) {
+    let html = ''
+    for (let i in lista) {
+        html += `<h3> ${lista[i].title} </h3>`
+        html += `${lista[i].body} </br>`
+        html += `<hr>`
+    }
+    document.querySelector('#posts').innerHTML = html
+}
